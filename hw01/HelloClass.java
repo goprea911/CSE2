bbfd6c8af992e1dc70689e07e035d0c7a2013dc8
goprea911 <gdo216@lehigh.edu>
2014-09-03 00:15:12 +0000

    HelloClass.java

---
+++ hw01/HelloClass.java
@@ -0,0 +1,30 @@
+//Gregory Oprea
+//9-2-2014
+//CSE2
+//hw01
+//Hello Class Java Program
+
+//  first compile the program
+//      javac HelloClass
+//  run the program
+//      java HelloClass
+
+//  define a class
+public class HelloClass{
+
+//  add main method
+  public static void main(String[] args) {
+
+//  print the statement with the box around it
+    System.out.println(" -------------");
+    System.out.println(" |  WELCOME  |");
+    System.out.println(" -------------");
+    System.out.println(" ^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-G--D--O--2--1--6-|");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println(" v  v  v  v  v  v");
+    System.out.println("I worked for 10 weeks last summer and barely saw the sunlight!");
+      
+  }
+}
