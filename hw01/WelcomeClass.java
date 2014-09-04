e24e4e73f2bebe85708851d44d7a256c0c6fab0c
goprea911 <gdo216@lehigh.edu>
2014-09-04 17:29:10 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Gregory Oprea
+//9-2-2014
+//CSE2
+//hw01
+//Welcome Class Java Program
+
+//  first compile the program
+//      javac WelcomeClass
+//  run the program
+//      java WelcomeClass
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+  public static void main(String[] args) {
+
+//  print the statement with the box around it
+    System.out.println(" -------------");
+    System.out.println(" |  WELCOME  |");
+    System.out.println(" -------------");
+    System.out.println(" ^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-G--D--O--2--1--6-|");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println(" v  v  v  v  v  v");
+    System.out.println("I worked for 10 weeks last summer and barely saw the sunlight!");
+      
+  }
+}
